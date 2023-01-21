7d0c6199b792428eae1fb9ca51a3f4dc2847d754
C-J-Sewell07 <55027220+C-J-Sewell07@users.noreply.github.com>
2023-01-21 07:18:52 -0600

    Added an index.js to separate html and js files. Also added a package.json files.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+import * as THREE from 'three';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
