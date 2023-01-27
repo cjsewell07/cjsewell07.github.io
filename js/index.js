193f96ad7772edb54ea186bf6c67a7ced19da2e7
C-J-Sewell07 <55027220+C-J-Sewell07@users.noreply.github.com>
2023-01-27 17:32:51 -0600

    Added a sphere with shading

--- js/index.js
+++ js/index.js
@@ -3,27 +3,35 @@ import WebGL from './WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight);
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometry = new THREE.SphereGeometry(1,32,16);
+const material = new THREE.MeshLambertMaterial({color: 0xffffff});
+const sphere = new THREE.Mesh(geometry, material);
+const lightDirectional = new THREE.DirectionalLight(0xffffff, 1);
 
 camera.position.z = 5;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
+scene.add(camera);
+scene.add(sphere);
+scene.add(lightDirectional);
+lightDirectional.position.set(5, 5, 5);
+
+// rendering the scene
+
+/* Functions */
 
 function animate(){
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //cube.rotation.x += 0.01;
+  //cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
 
+/* WebGL Check */
 if (WebGL.isWebGLAvailable()){
   animate();
 }
