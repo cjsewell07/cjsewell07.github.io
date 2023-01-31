07b1b375ea329f46e735942a3748b75a03fa3a31
C-J-Sewell07 <55027220+C-J-Sewell07@users.noreply.github.com>
2023-01-30 21:22:50 -0600

    Added a function to have the canvas resize correctly upon resizing the window.

--- js/index.js
+++ js/index.js
@@ -13,7 +13,13 @@ class Figure{
       ...params
     }
 
+    this.arms = [];
     this.group = new THREE.Group();
+    this.group.position.set(this.params.x, this.params.y, this.params.z);
+    //this.group.position.x = this.params.x;
+    //this.group.position.x = this.params.y;
+    //this.group.position.x = this.params.z;
+    this.group.rotation.y = this.params.ry;
     scene.add(this.group);
   }
 
@@ -46,11 +52,12 @@ class Figure{
       const box = new THREE.BoxHelper(armGroup, 0xffff00);
       this.group.add(armGroup);
       this.group.add(box);
-      arm.position.y = height * -0.75;
+      arm.position.y = height * -0.5;
       armGroup.position.x = m * 0.9;
 		  armGroup.position.y = 0.1;
       armGroup.rotation.z = degreesToRadians(10 * m);
       armGroup.add(arm);
+      this.arms.push(armGroup);
     }
     // back arm
     for(let i = 0; i < 2; i++){
@@ -65,12 +72,13 @@ class Figure{
 		  armGroup.position.y = 0.1;
       armGroup.rotation.z = degreesToRadians(30 * m);
       armGroup.add(arm);
+      this.arms.push(armGroup);
     }
   }
 
   createEyes(){
     const eyes = new THREE.Group();
-    const geometry = new THREE.SphereGeometry(0.15, 12, 8);
+    const geometry = new THREE.SphereGeometry(0.25, 12, 8);
     const material = new THREE.MeshLambertMaterial({color: 0x44445c});
     for(let i = 0; i < 2; i++){
       const eye = new THREE.Mesh(geometry, material);
@@ -79,28 +87,38 @@ class Figure{
       eye.position.x = 0.36 * m;
     }
     this.head.add(eyes);
-    eyes.position.y = -0.1;
-    eyes.position.z = 0.5;
+    eyes.position.y = 0;
+    eyes.position.z = 1;
   }
 
-  creatLegs(){
+  createLegs(){
     const legs = new THREE.Group();
     const geometry = new THREE.CylinderGeometry(0.25,0.25,2);
+    // forleg
     for(let i = 0; i < 2; i++){
       const leg = new THREE.Mesh(geometry, material);
       const m = i % 2 === 0 ? 1 : -1;
       legs.add(leg);
-      leg.position.x = m * 0.9;
+      leg.position.x = m * .5;
     }
+    // hindleg
+
     this.group.add(legs);
-    legs.position.y = .1;
-    this.body.add(legs);
+    legs.position.y = -2;
+    this.group.add(legs);
+  }
+
+  wave(){
+    this.arms.forEach((arm, index) =>{
+      //arm.rotation.y = this.params.armRotation;
+    })
   }
 
   init(){
     this.createBody();
     this.createHead();
     this.createArms();
+    this.createLegs();
   }
 }
 
@@ -112,12 +130,18 @@ const material = new THREE.MeshLambertMaterial({color: 0xffffff});
 const sphere = new THREE.Mesh(geometry, material);
 const lightDirectional = new THREE.DirectionalLight(0xffffff, 1);
 const lightAmbient = new THREE.AmbientLight(0x9eaeff, 0.2);
-const figure = new Figure();
 const degreesToRadians = (degrees) => {
   return degrees * (Math.PI / 180);
 }
+const figure = new Figure({
+  x: 0,
+  y: 0,
+  z: 0,
+  ry: 0
+});
+//new THREE.Box3().setFromObject(figure.group).getCenter(figure.group.position).multiplyScalar(-1)
 
-camera.position.z = 5;
+camera.position.z = 50;
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
@@ -132,11 +156,34 @@ figure.init();
 function animate(){
   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
+  //figure.group.rotation.x += 0.01;
+  //figure.group.rotation.y += 0.01;
+  if(figure.group.position.z == 40){
+    figure.group.position.z = 40;
+  }
+  else{
+    figure.group.position.z += 0.5;
+  }
 
   renderer.render(scene, camera);
 }
+
+function onWindowResize(){
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+gsap.to(figure.params, {
+  ry: degreesToRadians(360),
+  repeat: -1,
+  duration: 20
+})
+
+gsap.ticker.add(() => {
+  //figure.group.rotation.y = figure.params.ry;
+  figure.wave();
+})
 
 /* WebGL Check */
 if (WebGL.isWebGLAvailable()){
@@ -146,3 +193,5 @@ else{
   const warning = WebGL.getWebGLErrorMessage();
   document.getElementById('container').appendChild(warning);
 }
+
+window.addEventListener('resize', onWindowResize, false);
