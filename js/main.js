f639b46bde842c5d3b98cb9b3c4ec3e2cf7f0830
C-J-Sewell07 <55027220+C-J-Sewell07@users.noreply.github.com>
2023-11-15 07:11:48 -0600

    style: Created functions for initializing and other previous functions.

--- js/main.js
+++ js/main.js
@@ -1,53 +1,62 @@
 //import * as THREE from '../node_modules/three/build/three.module.js';
 import * as THREE from 'three'
-import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import WebGL from './WebGL.js';
+import {GLTFLoader} from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 import {FirstPersonControls} from '../node_modules/three/examples/jsm/controls/FirstPersonControls.js'
+import {PointerLockControls} from '../node_modules/three/examples/jsm/controls/PointerLockControls.js'
 
-//const controls = new OrbitControls( camera, renderer.domElement );
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-const lightDirectional = new THREE.DirectionalLight(0xffffff, 1);
-const lightAmbient = new THREE.AmbientLight(0x9eaeff, 0.2);
+let scene, camera, renderer, controls;
+let lightDirectional, lightAmbient;
 
-/* Setting user controls */
-const controls = new FirstPersonControls(camera, renderer.domElement);
-controls.movementSpeed = 1000;
-controls.lookSpeed = 0.1;
-
-/* Adding animations */
 let mixer;
 const loader = new GLTFLoader();
-loader.load('./assets/avatar_waving.glb', function(gltf){
-  scene.add(gltf.scene);
-  gltf.scene.rotation.y = 3.14;
-  mixer = new THREE.AnimationMixer(gltf.scene);
-  const action = mixer.clipAction(gltf.animations[0]);
-  action.play();
-}, undefined, function(error){
-  console.error(error);
-});
 
-/* Determining initial rendering from tutorial */
-camera.position.z = 5;
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
-document.body.appendChild(renderer.domElement);
-lightDirectional.position.set(0, 10, 5);
-scene.add(camera);
-scene.add(lightDirectional);
-scene.add(lightAmbient);
+init();
 
-/* Functions */
-//Animations
+function init(){
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  renderer = new THREE.WebGLRenderer();
+  lightDirectional = new THREE.DirectionalLight(0xffffff, 1);
+  lightAmbient = new THREE.AmbientLight(0x9eaeff, 0.2);
+  controls = new PointerLockControls(camera, renderer.domElement);
+
+  /* Determining initial rendering from tutorial */
+  camera.position.z = 5;
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  document.body.appendChild(renderer.domElement);
+  lightDirectional.position.set(0, 10, 5);
+
+  addAnimation();
+  addScene();
+}
+
+/* Adding animations */
+function addAnimation(){
+  loader.load('./assets/avatar_waving.glb', function(gltf){
+    scene.add(gltf.scene);
+    gltf.scene.rotation.y = 3.14;
+    mixer = new THREE.AnimationMixer(gltf.scene);
+    const action = mixer.clipAction(gltf.animations[0]);
+    action.play();
+  }, undefined, function(error){
+    console.error(error);
+  });
+}
+
+function addScene(){
+  scene.add(camera);
+  scene.add(lightDirectional);
+  scene.add(lightAmbient);
+}
+
 function animate(){
   requestAnimationFrame( animate );
   if (mixer){
     mixer.update(1/60);
   }
-  //controls.update(clock.getDelta());
-  controls.update(1/60);
+
   renderer.render(scene, camera);
 }
 
@@ -55,6 +64,7 @@ function animate(){
 function onWindowResize(){
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
+  //controls.handleResize();
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 window.addEventListener('resize', onWindowResize, false);
