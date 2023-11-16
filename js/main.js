023259a8c7471946742e8f035b358a4223638d6c
C-J-Sewell07 <55027220+C-J-Sewell07@users.noreply.github.com>
2023-11-15 19:38:02 -0600

    style: Moved the control instructions to be part of the Three.js world view.

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function init(){
 
   const blocker = document.getElementById( 'blocker' );
   const instructions = document.getElementById( 'instructions' );
-    instructions.addEventListener( 'click', function () {
+  instructions.addEventListener( 'click', function () {
     controls.lock();
   } );
 
